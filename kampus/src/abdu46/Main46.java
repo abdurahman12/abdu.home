1eebde054bfb02b4c94850a9d019b1b3a7a2d913
123 <magomedovabdurahman518@gmail.com>
2021-02-07 21:57:26 +0300

    40

---
+++ kampus/src/abdu46/Main46.java
@@ -0,0 +1,12 @@
+package abdu46;
+
+public class Main46 {
+    public static void main(String[] args) {
+        double a=8;
+        double b=3;
+        while (a>b){
+            a=a-b;
+            System.out.println(a);
+        }
+    }
+}
