814d9d404011b7d8c8783dfe5e321aac005710a9
123 <magomedovabdurahman518@gmail.com>
2021-02-07 07:03:37 +0300

    12

--- kampus/src/abdu40/Main40.java
+++ kampus/src/abdu40/Main40.java
@@ -2,6 +2,11 @@ package abdu40;
 
 public class Main40 {
     public static void main(String[] args) {
+        System.out.println("n=");
+         double sum = 0;
+         for (int i = 1; i <=i;i++ );
+sum =sum + 1 ;
+        System.out.println("Sum=" + sum );
 
     }
 }
---
+++ kampus/src/abdu41/Main41.java
@@ -0,0 +1,7 @@
+package abdu41;
+
+public class Main41 {
+    public static void main(String[] args) {
+
+    }
+}
